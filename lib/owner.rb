b17833a73b0c706dddf4fe99ece704b296537704
Levik Naparstek <levik010@gmail.com>
2019-03-20 17:47:37 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,7 +2,7 @@ class Owner
   attr_reader :species
   attr_accessor :name, :pets, :fish, :dog, :cat
   @@all = []
-  @@pets = {:fishes => [], :dogs => [], :cats => []}
+  # @@pets = {:fishes => [], :dogs => [], :cats => []}
   def initialize(species)
     @species = species
     @name = name
