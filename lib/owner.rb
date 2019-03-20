b88861bb0858ea78a087c8138708fe1432550e00
Levik Naparstek <levik010@gmail.com>
2019-03-20 17:49:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -41,11 +41,12 @@ class Owner
     end
     def play_with_cats
     # @@pets[:cats].each do |cat|
+    pets[:cats].each do |cat|
       cat.mood = "happy"
     end
   end
     def feed_fish
-    @@pets[:fishes].each do |fish|
+    # @@pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
   end
